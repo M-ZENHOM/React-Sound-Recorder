af3070a396ac40c97e456ef0eb6aad36577a3a1c
ZENHOM <zenhom.dev@gmail.com>
2024-07-20 03:08:05 +0300

    feat: add real example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { AudioRecorder } from 'react-sound-recorder';
+
 function App() {
-    return <h2>React Sound Recorder</h2>;
+    const [audioUrl, setAudioUrl] = useState<{ url: string; title: string }>();
+    const getAudio = (blob: Blob, title?: string) => {
+        const url = URL.createObjectURL(blob);
+        setAudioUrl({
+            url,
+            title: title!
+        });
+    };
+
+    return (
+        <>
+            <h2>React Sound Recorder</h2>
+            <AudioRecorder
+                onRecordingComplete={(blob, title) => getAudio(blob, title)}
+                askForTitle
+                timeLimit={10}
+                customControls={(actions, time, status) => (
+                    <div>
+                        <h2>{time}</h2>
+                        <button disabled={status === 'inactive'} onClick={actions.pauseRecording}>
+                            Pause
+                        </button>
+                        <button onClick={actions.stopRecording}>Stop</button>
+                        <button onClick={actions.startRecording}>Start</button>
+                    </div>
+                )}
+            />
+            {audioUrl && (
+                <>
+                    <h2>{audioUrl.title}</h2>
+                    <audio src={audioUrl.url} title={audioUrl.title} controls />
+                </>
+            )}
+        </>
+    );
 }
 
 export default App;
